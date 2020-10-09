ed032e618dae6210e9111fe0670967bc72b46d62
DaniPinelli <elidaniypri@gmail.com>
2020-10-09 15:09:31 -0300

    Practica Node

---
+++ app.js
@@ -0,0 +1,21 @@
+//cpu info 
+const os = require('os');
+
+let cpu = os.cpus();
+let sistema = os.platform();
+let usuario = os.hostname();
+
+console.log(cpu);
+console.log(sistema);
+console.log(usuario);
+
+
+
+let fs = require('fs');
+let cpu_string = JSON.stringify(cpu);
+
+fs.appendFile('node.txt', `Info cpu: ${cpu_string}`, function (error) {
+    if (error) {
+        console.log('Error al crear archivo');
+    }
+});
